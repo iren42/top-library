f50441ce12213e81710ede0fae6b6073b7405795
iren42 <isabelleren618@gmail.com>
2025-02-14 14:47:34 +0100

    feat: show books in browser

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const myLibrary = [];
+const bookStatus = ["read", "currently_reading", "to_read"];
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#showDialog");
+const closeButton = document.querySelector("button[value='close']");
+const form = document.querySelector("#book_form");
+const library = document.querySelector("#library");
 
 function Book(title, author, nb_pages, read)
 {
@@ -16,37 +23,6 @@ Book.prototype.info = function ()
     return (this.title + " by " + this.author + ", " + this.nb_pages + " has not been read yet");
 }
 
-function addBookToLibrary(library, book)
-{
-    // take params, create a book then store it in the array
-    if (Array.isArray(library))
-        library.push(book);
-}
-
-let thehobbit = new Book("The Hobbit", "Tolkien", 430, true);
-let thelightshop = new Book("The Light Shop", "IDK", 230, false);
-
-addBookToLibrary(myLibrary, thehobbit);
-addBookToLibrary(myLibrary, thelightshop);
-
-function displayLibrary()
-{
-    for (const key in myLibrary)
-    {
-        if (Object.prototype.hasOwnProperty.call(myLibrary, key))
-        {
-            const element = myLibrary[key];
-            console.log(element);
-        }
-    }
-}
-
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#showDialog");
-const closeButton = document.querySelector("button[value='cancel']");
-const form = document.querySelector("#book_form");
-const addButton = document.querySelector("button[type='submit']");
-
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () =>
 {
@@ -56,23 +32,76 @@ showButton.addEventListener("click", () =>
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () =>
 {
+    clearInputForm();
     dialog.close();
 });
 
 form.addEventListener("submit", event =>
 {
     event.preventDefault();
-    getData(form);
+    let newBook = getData(form);
+    addBookToLibrary(myLibrary, newBook);
+    clearInputForm();
+    dialog.close();
 });
+
+function addBookToLibrary(library, book)
+{
+    // take params, create a book then store it in the array
+    if (!Array.isArray(library))
+        return ;
+    library.push(book);
+    addBookToDOM(book);
+}
+
+function addBookToDOM(book)
+{
+    const domEl = document.createElement("div");
+    for (const property in book)
+    {
+        if (Object.prototype.hasOwnProperty.call(book, property)) 
+        {
+            const element = book[property];
+            domEl.textContent += element + " ";
+        }
+    }
+    if (domEl.textContent !== "")
+        document.querySelector("#library").append(domEl);
+}
+
+let thehobbit = new Book("The Hobbit", "Tolkien", 430, bookStatus[0]);
+let thelightshop = new Book("The Light Shop", "IDK", 230, bookStatus[1]);
+
+addBookToLibrary(myLibrary, thehobbit);
+addBookToLibrary(myLibrary, thelightshop);
+
+// function displayLibrary()
+// {
+//     for (const book in myLibrary)
+//     {
+//         if (Object.prototype.hasOwnProperty.call(myLibrary, book))
+//         {
+//             const aBook = myLibrary[book];
+//             console.log(book);
+//             displayBook(aBook);
+//         }
+//     }
+// }
+
+function clearInputForm(){
+    document.querySelector("#book_form").reset();
+}
 
 function getData(form)
 {
-    var formData = new FormData(form);
+    let formData = new FormData(form);
+    let buffer = [];
 
-    for (var pair of formData.entries())
+    for (let pair of formData.entries())
     {
-        console.log(pair[0] + ": " + pair[1]);
+        // console.log(pair[0] + ": " + pair[1]);
+        buffer.push(pair[1]);
     }
-
-    console.log(Object.fromEntries(formData));
+    let newBook = new Book(buffer[0], buffer[1], Number(buffer[2]), buffer[3]);
+    return (newBook);
 }
